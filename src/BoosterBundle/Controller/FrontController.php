09824c17e9fd18dd0219b8b92b06cebfef41bb13
Yoann <yoann.gloaguen@gmail.com>
2016-11-22 17:19:44 +0100

    creation of paths for every footer's links: 	- 'cgv' 	- 'faq' 	- 'la charte' 	- 'mentions legales' 	- 'l'equipe' 	- 'contact'

---
+++ src/BoosterBundle/Controller/FrontController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace BoosterBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class FrontController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('BoosterBundle:Front:index.html.twig');
+    }
+    public function cgvAction()
+    {
+        return $this->render('BoosterBundle:Front:cgv.html.twig');
+    }
+    public function faqAction()
+    {
+        return $this->render('BoosterBundle:Front:faq.html.twig');
+    }
+    public function charteAction()
+    {
+        return $this->render('BoosterBundle:Front:charte.html.twig');
+    }
+    public function mentions_legales_Action()
+    {
+        return $this->render('BoosterBundle:Front:mentions_legales.html.twig');
+    }
+    public function equipeAction()
+    {
+        return $this->render('BoosterBundle:Front:equipe.html.twig');
+    }
+    public function contactAction()
+    {
+        return $this->render('BoosterBundle:Front:contact.html.twig');
+    }
+}
