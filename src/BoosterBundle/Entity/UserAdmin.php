e95203f670ccfcc4835f48cc58e801bc15519a30
lauriegandon <laurie.gandon@gmail.com>
2016-11-22 18:14:27 +0100

    created file UserAdmin.orm.yml

--- src/BoosterBundle/Entity/UserAdmin.php
+++ src/BoosterBundle/Entity/UserAdmin.php
@@ -8,8 +8,28 @@
 
 namespace BoosterBundle\Entity;
 
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
 
-class UserAdmin
+
+/**
+ * Class UserAdmin
+ * @package BoosterBundle\Entity
+ * @ORM\Entity
+ * @ORM\Table(name="user_admin")
+ */
+class UserAdmin extends BaseUser
 {
+    /**
+     * @var integer
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
 
+    public function __construct()
+    {
+        parent::__construct();
+    }
 }
