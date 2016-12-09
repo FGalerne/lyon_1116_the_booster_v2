1f0c2aeac21ae3692017c6276778f6c3efd4fe57
lauriegandon <laurie.gandon@gmail.com>
2016-12-09 12:17:02 +0100

    configuration access page edit dashboard

--- src/BoosterBundle/Controller/DashboardController.php
+++ src/BoosterBundle/Controller/DashboardController.php
@@ -10,6 +10,10 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DashboardController extends Controller
 {
+    /**
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
     public function societyAction($id)
     {
         $em = $this->getDoctrine()->getManager();
@@ -29,6 +33,34 @@ class DashboardController extends Controller
         return $this->redirectToRoute('booster_charte');
     }
 
+    /**
+     * @param Request $request
+     * @param Society $society
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function societyEditAction(Request $request, Society $society)
+    {
+        $editForm = $this->createForm('BoosterBundle\Form\SocietyType', $society);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('dashboard_society',
+                array('id' => $society->getId()));
+        }
+
+        return $this->render('@Booster/Dashboard/dashboard-booste-edit.html.twig', array(
+            'id'    =>  $society->getId(),
+            'society' => $society,
+            'edit_form' => $editForm->createView(),
+        ));
+    }
+
+    /**
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
     public function boosterAction($id)
     {
         $em = $this->getDoctrine()->getManager();
@@ -45,6 +77,11 @@ class DashboardController extends Controller
         return $this->redirectToRoute('booster_charte');
     }
 
+    /**
+     * @param Request $request
+     * @param Booster $booster
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
     public function boosterEditAction(Request $request, Booster $booster)
     {
         $editForm = $this->createForm('BoosterBundle\Form\BoosterType', $booster);
