4b494e6257a1bbab14c06ea7cfd2da9e97eb473d
Riad Hacini <riad.hacini.pro@gmail.com>
2017-01-10 18:03:20 +0100

    solved the double apparition of the variable

--- src/BoosterBundle/Repository/ProjectRepository.php
+++ src/BoosterBundle/Repository/ProjectRepository.php
@@ -12,15 +12,17 @@ class ProjectRepository extends \Doctrine\ORM\EntityRepository
 
     //function that get the Project By Society in the twig view.
 
-    public function getProjectBySociety($value)
+    public function getProjectBySociety($id, $status)
     {
         $req = $this->createQueryBuilder('p')
-            ->select('p.projectName, p.status, p.givenTime')   // here the fields we want to get
-            ->innerJoin('p.society','s')                       // here the join field we want to use
-            ->where('p.society = :id')                         // :id means (id is a parameter)
-            ->setParameter('id', $value)                       // declaration of the parameter
+            ->select('p.projectName, p.status, p.givenTime, p.creationStatus, p.category')   // here the fields we want to get
+            ->innerJoin('p.society','s')                                                     // here the join field we want to use
+            ->where('p.society = :id')                                                       // :id means (id is a parameter)
+            ->andWhere('p.status = Accepte')                                                 // need to found it's equals to what
+            ->setParameter('id', $id)                                                        // declaration of the parameter
+            ->setParameter('status', $status)                                                // declaration of the parameter
             ->getQuery();
 
-        return $req->getScalarResult();
+        return $req->getResult();
     }
 }
