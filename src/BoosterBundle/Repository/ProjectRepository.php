1d614ad0f1fe2db904ccea1a45d7fac3ddcd6ced
Riad Hacini <riad.hacini.pro@gmail.com>
2017-01-10 16:40:37 +0100

    Documentation of Project Repository

--- src/BoosterBundle/Repository/ProjectRepository.php
+++ src/BoosterBundle/Repository/ProjectRepository.php
@@ -15,10 +15,10 @@ class ProjectRepository extends \Doctrine\ORM\EntityRepository
     public function getProjectBySociety($value)
     {
         $req = $this->createQueryBuilder('p')
-            ->select('p.projectName, p.status, p.givenTime')   //ici on renseigne les champs qu'on veut récupérer
-            ->innerJoin('p.society','s')
-            ->where('p.society = :id')
-            ->setParameter('id', $value)
+            ->select('p.projectName, p.status, p.givenTime')   // here the fields we want to get
+            ->innerJoin('p.society','s')                       // here the join field we want to use
+            ->where('p.society = :id')                         // :id means (id is a parameter)
+            ->setParameter('id', $value)                       // declaration of the parameter
             ->getQuery();
 
         return $req->getScalarResult();
