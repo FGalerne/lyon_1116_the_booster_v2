40f1fd10a834314553b23562b1e72ca718247b33
Riad Hacini <riad.hacini.pro@gmail.com>
2017-01-11 11:23:05 +0100

    change the look of the dashboard society

--- src/BoosterBundle/Repository/ProjectRepository.php
+++ src/BoosterBundle/Repository/ProjectRepository.php
@@ -12,15 +12,13 @@ class ProjectRepository extends \Doctrine\ORM\EntityRepository
 
     //function that get the Project By Society in the twig view.
 
-    public function getProjectBySociety($id, $status)
+    public function getProjectBySociety($id)
     {
         $req = $this->createQueryBuilder('p')
             ->select('p.projectName, p.status, p.givenTime, p.creationStatus, p.category')   // here the fields we want to get
             ->innerJoin('p.society','s')                                                     // here the join field we want to use
             ->where('p.society = :id')                                                       // :id means (id is a parameter)
-            ->andWhere('p.status = Accepte')                                                 // need to found it's equals to what
             ->setParameter('id', $id)                                                        // declaration of the parameter
-            ->setParameter('status', $status)                                                // declaration of the parameter
             ->getQuery();
 
         return $req->getResult();
