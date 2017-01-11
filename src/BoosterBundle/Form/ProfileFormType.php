95957ce1f2d6becd5add32520c290337f212833a
Wingelo <cedric.gamrat@gmail.com>
2017-01-11 17:07:58 +0100

    Fosuser forgetten  password + html fos + + routing + charte

--- src/BoosterBundle/Form/ProfileFormType.php
+++ src/BoosterBundle/Form/ProfileFormType.php
@@ -8,8 +8,11 @@
 
 namespace BoosterBundle\Form;
 
+use BoosterBundle\Form\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class ProfileFormType extends AbstractType
@@ -18,9 +21,29 @@ class ProfileFormType extends AbstractType
     {
         parent::buildForm($builder, $options);
         $builder
-            ->remove('username');
+			->add('last_name', TextType::class,array (
+				'required' => true,
+			))
+			->add('first_name', TextType::class,array (
+		'required' => true,))
+			->add('title', ChoiceType::class, array(
+				'choices' => array(
+					'MR' => "Mr",
+					'MME' => "Mme",
+				),
+			))
+			->add('professional_function', TextType::class,array (
+				'required' => true,))
+			->add('phone', new TelType(), array(
+				'required' => false,
+				'pattern' => "^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$"
+			))
+			->remove('username')
+        	->remove('email')
+        	->remove('current_password');
 
     }
+
 
 
     public function getParent()
@@ -32,4 +55,12 @@ class ProfileFormType extends AbstractType
     {
         return 'app_user_profile';
     }
+	protected function buildUserForm(FormBuilderInterface $builder, array $options)
+	{
+		$builder
+
+			->add('username', null, array('label' => 'form.username', 'translation_domain' => 'FOSUserBundle'))
+			->remove('email', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\EmailType'), array('label' => 'form.email', 'translation_domain' => 'FOSUserBundle'))
+		;
+	}
 }
